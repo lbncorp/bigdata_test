4c1331d2b3a59864380b3d9bdb7d4d3f6b2e6674
Umaru Lamin <ulamin97@gmail.com>
2017-05-03 22:40:02 -0700

    add commit

--- Umaru.java
+++ Umaru.java
@@ -8,9 +8,7 @@ import static java.lang.System.out;
 public class Umaru {
 public static void main(String[] args){
 
-//System.out.println("This is just to test github");
-//System.out.println("This is Umaru's name");
-
+System.out.println("This is Umaru");
 
 }
 }
