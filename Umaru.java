9afd59b1ca45a6343a328031a8066ffec53c7d88
Umaru Lamin <ulamin97@gmail.com>
2017-05-03 20:21:14 -0700

    Adding Files

---
+++ Umaru.java
@@ -0,0 +1,14 @@
+
+/*public class HelloWorld {
+    public static void main(String[] args) 
+
+import static java.lang.System.out;
+*/
+
+public class Umaru {
+public static void main(String[] args){
+
+System.out.println("This is just to test github");
+
+}
+}
