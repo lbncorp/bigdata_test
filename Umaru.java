c7f7789d296dbfa435549ceac6f11a83e9ed44df
Umaru Lamin <ulamin97@gmail.com>
2017-05-08 17:36:02 -0700

    Did this committ comment go

--- Umaru.java
+++ Umaru.java
@@ -8,6 +8,10 @@ import static java.lang.System.out;
 public class Umaru {
 public static void main(String[] args){
 
+
+System.out.println("This is Umaru with added data");
+System.out.println("This is Umaru with added data");
+System.out.println("This is Umaru with added data");
 System.out.println("This is Umaru with added data");
 
 }
