282e9cb4b578d123ffb69de2ccbde522ce05a965
Umaru Lamin <ulamin97@gmail.com>
2017-05-08 17:29:54 -0700

    This is Umaru file

--- Umaru.java
+++ Umaru.java
@@ -8,7 +8,7 @@ import static java.lang.System.out;
 public class Umaru {
 public static void main(String[] args){
 
-System.out.println("This is Umaru");
+System.out.println("This is Umaru with added data");
 
 }
 }
