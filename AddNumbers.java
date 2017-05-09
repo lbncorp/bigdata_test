b467c000b98981576c220270b5f752fa9198ec73
Umaru Lamin <ulamin97@gmail.com>
2017-05-08 19:26:11 -0700

    Numbers added

--- AddNumbers.java
+++ AddNumbers.java
@@ -3,8 +3,16 @@ public static void main(String[] args){
 
 
 int number=1;
+int number1=5;
+int total=0;
+total = number + number1;
 
-System.out.println("This is number is" + number);
+System.out.println("This is number is " + number);
+System.out.println("This is number1 is " + number1);
+System.out.println("This total is " + total);
+
+
+
 
 }
 }
